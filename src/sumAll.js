8d5f1101fa5c0b0669eca93d560d4a10230213d4
Hanna Senkevich <hanna.senkevich@gmail.com>
2016-10-04 08:30:34 +0200

    Replaced ES6 spread operator by apply method in sumAll

--- src/sumAll.js
+++ src/sumAll.js
@@ -1,7 +1,7 @@
 function sumAll(arr) {
     var sum = 0;
-    var start = Math.min(...arr);
-    var end = Math.max(...arr);
+    var start = Math.min.apply(null, arr);
+    var end = Math.max.apply(null, arr);
 
     for (var i=start; i<=end; i++) {
         sum += i;
