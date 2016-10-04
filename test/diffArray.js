45e930b792e04b5441522c912115a45340ffa588
Hanna Senkevich <hanna.senkevich@gmail.com>
2016-10-04 19:46:44 +0200

    More readable tests from diffArray

--- test/diffArray.js
+++ test/diffArray.js
@@ -4,28 +4,60 @@ var assert = chai.assert;
 var diffArray = require('../src/diffArray.js');
 
 it('diffArray', function() {
-    assert(typeof diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) === "object",
-        'message: <code>diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])</code> should return an array.');
-    assert.sameMembers(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
-            ["diorite", "andesite", "grass", "dirt", "dead shrub"]), ["pink wool"],
-        'message: <code>["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ' +
-        '["diorite", "andesite", "grass", "dirt", "dead shrub"]</code> should return <code>["pink wool"]</code>.');
-    assert.sameMembers(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"],
-            ["diorite", "andesite", "grass", "dirt", "dead shrub"]), ["diorite", "pink wool"],
-        'message: <code>["andesite", "grass", "dirt", "pink wool", "dead shrub"], ' +
-        '["diorite", "andesite", "grass", "dirt", "dead shrub"]</code> should return <code>["diorite", "pink wool"]</code>.');
-    assert.sameMembers(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]), [],
-        'message: <code>["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]</code> ' +
-        'should return <code>[]</code>.');
-    assert.sameMembers(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]), [4],
-        'message: <code>[1, 2, 3, 5], [1, 2, 3, 4, 5]</code> should return <code>[4]</code>.');
-    assert.sameMembers(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]), ["piglet", 4],
-        'message: <code>[1, "calf", 3, "piglet"], [1, "calf", 3, 4]</code> should return <code>["piglet", 4]</code>.');
-    assert.sameMembers(diffArray([], ["snuffleupagus", "cookie monster", "elmo"]), ["snuffleupagus", "cookie monster", "elmo"],
-        'message: <code>[], ["snuffleupagus", "cookie monster", "elmo"]</code> should return <code>' +
-        '["snuffleupagus", "cookie monster", "elmo"]</code>.');
-    assert.sameMembers(diffArray([1, "calf", 3, "piglet"], [7, "filly"]), [1, "calf", 3, "piglet", 7, "filly"],
-        'message: <code>[1, "calf", 3, "piglet"], [7, "filly"]</code> should return <code>[1, "calf", 3, "piglet", 7, "filly"]</code>.');
+    assert(
+        typeof diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) === 'object',
+        'diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.'
+    );
+
+    assert.sameMembers(
+        diffArray(
+            ['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+            ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+        ),
+        ['pink wool'],
+        '["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ' +
+        '["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].'
+    );
+
+    assert.sameMembers(
+        diffArray(
+            ['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+            ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+        ),
+        ['diorite', 'pink wool'],
+        '["andesite", "grass", "dirt", "pink wool", "dead shrub"], ' +
+        '["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].'
+    );
+
+    assert.sameMembers(
+        diffArray(['andesite', 'grass', 'dirt', 'dead shrub'], ['andesite', 'grass', 'dirt', 'dead shrub']),
+        [],
+        '["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].'
+    );
+
+    assert.sameMembers(
+        diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]),
+        [4],
+        '[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].'
+    );
+
+    assert.sameMembers(
+        diffArray([1, 'calf', 3, 'piglet'], [1, 'calf', 3, 4]),
+        ['piglet', 4],
+        '[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].'
+    );
+
+    assert.sameMembers(
+        diffArray([], ['snuffleupagus', 'cookie monster', 'elmo']),
+        ['snuffleupagus', 'cookie monster', 'elmo'],
+        '[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].'
+    );
+
+    assert.sameMembers(
+        diffArray([1, 'calf', 3, 'piglet'], [7, 'filly']),
+        [1, 'calf', 3, 'piglet', 7, 'filly'],
+        '[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].'
+    );
 });
 
 it('diffArray shoudn\'t modify arguments', function() {
