e77c493b101523914837c957371afbbeb6fbc30c
Hanna Senkevich <hanna.senkevich@gmail.com>
2016-11-24 10:31:59 +0100

    Small optimisation for dropElements

--- src/dropElements.js
+++ src/dropElements.js
@@ -1,12 +1,11 @@
 function dropElements(arr, func) {
-    var result = [];
 
     for (var i=0; i< arr.length; i++) {
         if (func(arr[i])) {
             return arr.slice(i);
         }
     }
-    return result;
+    return [];
 }
 
 module.exports = dropElements;
